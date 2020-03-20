78e1607820ef74f3ca015682abb5abb7790d2be8
James Trombo <jamestrombo@gmail.com>
2020-03-20 00:02:21 -0400

    massive scroll changes

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
 headerup = false;
-toggled = false;
+toggled = 0;
 window.addEventListener('scroll', function(e){
-	if (window.scrollY >= 50){
+	console.log(headerup);
+	if (window.scrollY > 50 && toggled == 0){
 		headerup = true;
 	}
-	if (headerup == true && toggled == false){
-		toggled = true;
+	if (window.scrollY < 50 && toggled == 1) {
+		headerup = false;
+	}
+	if (headerup && toggled == 0){
+		raiseHeader();
+	} 
+	if (headerup === false && toggled == 1){
+		lowerHeader();
+	}
+
+});
+function raiseHeader() {
+		toggled = toggled + 1;
+		console.log("bitch");
 		$("#header").velocity({
 			height: "100px",
 		}, {
-			duration: 2000,
+			duration: 200,
 		});
 		$("#name").velocity({
 			fontSize: "35px",
 		}, {
-			duration: 2000,
+			duration: 200,
 		});
 		$("#bar").velocity({
 			width: "400px",
 			height: "0.5vh",
 		}, {
-			duration: 2000,
+			duration: 200,
 		});
-	} 
-});
+}
+function lowerHeader(){
+		toggled = toggled - 1;
+		console.log("shit");
+		$("#header").velocity({
+			height: "150px",
+		}, {
+			duration: 200,
+		});
+		$("#name").velocity({
+			fontSize: "60px",
+		}, {
+			duration: 200,
+		});
+		$("#bar").velocity({
+			width: "700px",
+			height: "1vh",
+		}, {
+			duration: 200,
+		});
+}
